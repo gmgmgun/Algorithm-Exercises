80bdf1b3e144d4736f0112d6b92a6822208af1c4
gmgmgun <gmgmgun@gmail.com>
2023-05-22 21:45:59 +0900

    HackerRank/SalesbyMatch_v2

---
+++ HackerRank/SalesbyMatch_v2.js
@@ -0,0 +1,65 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n, ar) {
+  const countMap = new Map();
+  let totalPairs = 0;
+
+  for (const color of ar) {
+    const count = (countMap.get(color) || 0) + 1;
+    countMap.set(color, count);
+
+    if (count % 2 === 0) {
+      totalPairs++;
+    }
+  }
+
+  return totalPairs;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const ar = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arTemp) => parseInt(arTemp, 10));
+
+  const result = sockMerchant(n, ar);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}

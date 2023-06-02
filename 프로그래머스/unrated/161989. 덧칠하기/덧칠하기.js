343fbec55d4fd09bde8fdd0e3bf481a02f33ef98
Dongmin Lee <65591303+gmgmgun@users.noreply.github.com>
2023-06-02 14:59:18 +0900

    Update 덧칠하기.js

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -2,16 +2,21 @@ function solution(n, m, section) {
     const wall = Array(n).fill(true);
     let index = 0;
     let cnt = 0;   
+    let falseCnt = 0;
 
     for (let i=0; i<section.length; i++) {
         wall[section[i]-1] = false;
+        falseCnt++;
     }
     
-    while(wall.indexOf(false) !== -1) {
+    while(falseCnt > 0) {
         if(wall[index] === false) {
             cnt++;
-            for(let i=index; i<index+m; i++) {
-                wall[i]=true; 
+            for(let i=index; i<index+m && i < n; i++) {
+                if(!wall[i]){
+                    wall[i]=true;
+                    falseCnt--;
+                }
             }
         }
         index++;

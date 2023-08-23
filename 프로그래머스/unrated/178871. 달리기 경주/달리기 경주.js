91fecd50e0ba6da460ada73ec1959f2f722b418e
Dongmin Lee <65591303+gmgmgun@users.noreply.github.com>
2023-08-23 15:14:01 +0900

    [unrated] Title: 달리기 경주, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,24 @@
+function solution(players, callings) {
+  const indexMap = {};
+
+  for (let i = 0; i < players.length; i++) {
+    indexMap[players[i]] = i;
+  }
+
+  for (let i = 0; i < callings.length; i++) {
+    const calling = callings[i];
+    const curIdx = indexMap[calling];
+    const prevIdx = curIdx - 1;
+
+    if (prevIdx >= 0) {
+      [players[curIdx],players[prevIdx]] = [players[prevIdx],players[curIdx]];
+      indexMap[calling] = prevIdx;
+      indexMap[players[curIdx]] = curIdx;
+    }
+  }
+
+  return players;
+}
+
+
+

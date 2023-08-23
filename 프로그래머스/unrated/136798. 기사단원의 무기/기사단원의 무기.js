ec30cf40599f3274f0668f5dd14bf241b0e9954e
Dongmin Lee <65591303+gmgmgun@users.noreply.github.com>
2023-08-23 15:11:58 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.39 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,14 +1,14 @@
 function solution(number, limit, power) {
     let answer = 0;
-    let yaksuArr = Array(number + 1).fill(1);
+    let yaksuArr = Array(number).fill(1);
 
     for (let i = 2; i <= number; i++) {
         for (let j = i; j <= number; j += i) {
-            yaksuArr[j]++;
+            yaksuArr[j-1]++;
         }
     }
 
-    answer = yaksuArr.slice(1).map(el => el > limit ? power : el).reduce((acc, val) => acc + val);
+    answer = yaksuArr.map(el => el > limit ? power : el).reduce((acc, val) => acc + val);
 
     return answer;
 }

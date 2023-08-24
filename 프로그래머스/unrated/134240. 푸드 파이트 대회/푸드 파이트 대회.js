158231a23c80a406ddc2a84e23cb54164f7dd721
Dongmin Lee <65591303+gmgmgun@users.noreply.github.com>
2023-08-24 13:57:02 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,15 @@
+function solution(food) {
+    var answer = '';
+    let arr = [0];
+    
+    for (let i = food.length-1; i >= 1 ; i--) {
+        for(let j = 0; j < parseInt(food[i]/2); j++) {
+            arr.push(i);
+            arr.unshift(i);
+        }
+    }
+    
+    answer = arr.toString().replaceAll(',','');
+    
+    return answer;
+}

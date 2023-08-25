933b5f0d1fad0929efa47e213d2c0bf1716bb413
Dongmin Lee <65591303+gmgmgun@users.noreply.github.com>
2023-08-25 16:00:31 +0900

    [unrated] Title: 콜라 문제, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,1 +1,15 @@
-solution = (a, b, n) => Math.floor(Math.max(n - b, 0) / (a - b)) * b
+function solution(a, b, n) {
+    
+    function getNewCoke(n,newCoke) {
+        if(n < a) {
+            return newCoke;
+        }
+        
+        newCoke += parseInt(n / a) * b;
+        n = parseInt(n / a) * b + n%a;
+        
+        return getNewCoke(n,newCoke);
+    }
+    
+    return getNewCoke(n,0) ;
+}

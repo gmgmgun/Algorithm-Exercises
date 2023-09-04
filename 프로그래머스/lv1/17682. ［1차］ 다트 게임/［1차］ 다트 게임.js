254cdca03acb47e720e77b31cf8b0c06f2d57b94
Dongmin Lee <65591303+gmgmgun@users.noreply.github.com>
2023-09-04 17:03:26 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,28 +1,22 @@
 function solution(dartResult) {
-    var scores = [];
-    var score = 0;
-    var idx = -1;
-    
-    dartResult = dartResult.match(/(\d+|[^\d])/g);
+    var answer = [];
+    var res = 0;
     
     for(let i = 0; i < dartResult.length; i++) {
         if(Number.isInteger(Number(dartResult[i]))) {
-            if(i) {
-                scores.push(score);
-                idx++;
-            }
-            score = Number(dartResult[i]);
+            if(i) answer.push(res);
+            res = Number(dartResult[i]);
         }
-        else if (dartResult[i] === 'D') score = score**2;
-        else if (dartResult[i] === 'T') score = score**3;
+        else if (dartResult[i] === 'D') res **= res;
+        else if (dartResult[i] === 'T') res = res*res*res;
         else if (dartResult[i] === '*') {
-            score = score*2;
-            if(scores[idx]) scores[idx] = scores[idx]*2
+            res = res*2;
+            if(answer[i-1]) answer[i-1] = answer[i-1]*2
         }
-        else if (dartResult[i] === '#') score = score*(-1);
+        else if (dartResult[i] === '#') res = res*(-1);
     }
-    
-    if (i = dartResult.length) scores.push(score);
-    
-    return scores.reduce((acc,cur) => acc+cur);
+    if (i = dartResult.length) answer.push(res);
+    console.log(answer)
+    const sum = answer.reduce((acc,cur) => acc+cur)
+    return sum;
 }

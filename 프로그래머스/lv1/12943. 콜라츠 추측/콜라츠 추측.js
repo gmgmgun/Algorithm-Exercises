7aebf93c7cb3fe85865dbb1d04069035c47549de
Dongmin Lee <65591303+gmgmgun@users.noreply.github.com>
2023-09-07 10:20:06 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,9 @@
+const solution = (num) => {
+    let cnt = 0;
+    while(num > 1) {
+        if(cnt === 500) return -1;
+        num%2 ? num=num*3+1 : num=num/2
+        cnt++;
+    }
+    return cnt;
+}

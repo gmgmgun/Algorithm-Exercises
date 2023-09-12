49b6aad7b2908d2611acbe49ad17064a4f239877
Dongmin Lee <65591303+gmgmgun@users.noreply.github.com>
2023-09-12 09:27:09 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,11 @@
+const solution = (a,b) => {
+    let max = Math.max(a,b);
+    let min = Math.min(a,b);
+    let sum = 0;
+    
+    for (let i = min; i <= max; i++) {
+        sum += i;    
+    }
+    
+    return sum;
+}

1e5dda96ef32cae1481a7749d754a210ba02b405
Dongmin Lee <65591303+gmgmgun@users.noreply.github.com>
2023-11-06 14:33:12 +0900

    [level unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -1,9 +1,4 @@
-const solution = (a, d, included) => {
+function solution(a, d, included) {
     var answer = 0;
-    
-    for(let i = 0; i < included.length; i++) {
-        if(included[i]) answer += (a+d*i); 
-    }
-    
     return answer;
 }

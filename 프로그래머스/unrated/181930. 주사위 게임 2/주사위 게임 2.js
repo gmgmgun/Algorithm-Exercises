8c06902dfeb7d67be3bfaa4a2dcf131a76514455
Dongmin Lee <65591303+gmgmgun@users.noreply.github.com>
2023-11-06 14:40:07 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,7 @@
+function solution(a, b, c) {
+    if (a===b && b===c) return (a+b+c) * (a*a+b*b+c*c) * (a*a*a+b*b*b+c*c*c); 
+    
+    if (a!==b && b!==c && a!==c) return (a+b+c);
+    
+    return (a+b+c) * (a*a+b*b+c*c);
+}

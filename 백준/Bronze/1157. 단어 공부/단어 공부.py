1533dc3d12d17085ec214feea469ec1a031c2f64
Dongmin Lee <65591303+gmgmgun@users.noreply.github.com>
2024-03-28 12:09:32 +0900

    [Bronze I] Title: 단어 공부, Time: 200 ms, Memory: 117600 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -2,17 +2,10 @@ from collections import Counter
 
 w = input()
 
-
 def find_most_frequent_char(word):
     char_dict = Counter(word.upper())
     max_freq = max(char_dict.values())
-    max_char = None
-    for char, freq in char_dict.items():
-        if freq == max_freq:
-            if max_char is not None:
-                return "?"
-            max_char = char
-    return max_char
-
+    max_chars = [char for char, freq in char_dict.items() if freq == max_freq]
+    return max_chars[0] if len(max_chars) == 1 else "?"
 
 print(find_most_frequent_char(w))

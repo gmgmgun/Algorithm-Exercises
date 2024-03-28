cb99bafabead06e3251b3c8304c84444373da48c
Dongmin Lee <65591303+gmgmgun@users.noreply.github.com>
2024-03-28 11:14:55 +0900

    [Bronze II] Title: 상수, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,9 +1,11 @@
 l = input().split()
 
-
 def find_largest_reversed(num_list):
-    reversed_list = [int(num[::-1]) for num in num_list]
-    return max(reversed_list)
-
+    max_val = 0
+    for num in num_list:
+        reversed_num = int(num[::-1])
+        if reversed_num > max_val:
+            max_val = reversed_num
+    return max_val
 
 print(find_largest_reversed(l))

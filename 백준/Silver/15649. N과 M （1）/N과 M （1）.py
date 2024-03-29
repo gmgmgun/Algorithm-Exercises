ac23add02a73d8affdba4d14d9a109b3acf11fdf
Dongmin Lee <65591303+gmgmgun@users.noreply.github.com>
2024-03-29 15:18:00 +0900

    [Silver III] Title: N과 M (1), Time: 188 ms, Memory: 112408 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,20 +1,18 @@
-def find_comb(n, m, li, used_set):
+def find_comb(n, m, li):
     if len(li) == m:
         print(' '.join(map(str, li)))
         return
 
     for i in range(1, n + 1):
-        if i not in used_set:
-            used_set.add(i)
+        if i not in li:
             li.append(i)
-            find_comb(n, m, li, used_set)
+            find_comb(n, m, li)
             li.pop()
-            used_set.remove(i)
 
 
 def find_all_combs():
     n, m = map(int, input().split())
-    find_comb(n, m, [], set())
+    find_comb(n, m, [])
 
 
 find_all_combs()

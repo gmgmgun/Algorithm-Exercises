4e69cf05c12ff88fd1019525bdae3b056ebfad8b
Dongmin Lee <65591303+gmgmgun@users.noreply.github.com>
2024-03-29 17:30:18 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 240 ms, Memory: 138856 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
@@ -0,0 +1,25 @@
+def find_bear_counts():
+    cnt_logs = int(input())
+    logs = [input().split() for _ in range(cnt_logs)]
+
+    logs_list = []
+    cur_logs = []
+    cnt_bear = 0
+
+    for log in logs:
+        if log == ['ENTER']:
+            if cur_logs:
+                logs_list.append(cur_logs)
+            cur_logs = []
+        else:
+            cur_logs.append(tuple(log))
+
+    if cur_logs:
+        logs_list.append(cur_logs)
+
+    for logs in logs_list:
+        cnt_bear += len(set(logs))
+
+    print(cnt_bear)
+
+find_bear_counts()

e035cff6468d7af79f1e0705ba8fe6adbc887adf
Dongmin Lee <65591303+gmgmgun@users.noreply.github.com>
2024-04-03 15:42:02 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 140 ms, Memory: 112004 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+n = input()
+a = list(map(int, input().split()))
+
+
+def pop_balloons(balloons):
+    pop_order = []
+    deck = deque((i + 1, m) for i, m in enumerate(balloons))
+
+    while deck:
+        index, move = deck.popleft()
+        pop_order.append(index)
+
+        if move > 0:
+            move -= 1
+            deck.rotate(-move)
+        else:
+            move *= -1
+            deck.rotate(move)
+
+    return ' '.join(map(str, pop_order))
+
+
+print(pop_balloons(a))

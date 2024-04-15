887073a03c5b7bf1d44efcd85ef749474d07e15c
Dongmin Lee <65591303+gmgmgun@users.noreply.github.com>
2024-04-15 12:19:39 +0900

    [Silver III] Title: 주유소, Time: 160 ms, Memory: 135428 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -8,8 +8,6 @@
 도로의 길이와 기름 가격을 입력 받았을 때, 끝->끝으로 가는 최소 비용을 구해라
 
 [접근]
-1. 최소 비용 = 최대 이득 구하는 거니까 그리디로 접근
-2. 일단 전체 거리만큼 만땅으로 채우고 주유소 도착할 때마다 가격 비교해서 비용을 업데이트를 해주자
 
 [카테고리]
 그리디
@@ -24,15 +22,15 @@ O = list(map(int, sys.stdin.readline().split()))
 
 
 def find_min_cost(n, c, o):
-    cur_distance = sum(c)
-    cur_cost = o[0] * cur_distance
+    min_cost = 0
+    current_min_price = o[0]
 
-    for i in range(1,n):
-        cur_distance -= c[i-1]
-        if o[i] < o[i-1]:
-            cur_cost = cur_cost + (o[i] - o[i-1]) * cur_distance
+    for i in range(n - 1):
+        if o[i] < current_min_price:
+            current_min_price = o[i]
+        min_cost += current_min_price * c[i]
 
-    return cur_cost
+    return min_cost
 
 
 print(find_min_cost(N, C, O))
